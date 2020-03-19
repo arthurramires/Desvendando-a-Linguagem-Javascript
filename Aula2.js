0690d20270acc3f82bdb7621d0b69a8afa1e470a
arthurramires <arthurramires1802@outlook.com>
2020-03-19 10:09:39 -0400

    --Aula 2-- Number

---
+++ Aula2.js
@@ -0,0 +1,13 @@
+//Aula 2 - Number
+
+var nota = 10;
+
+//Eleva o valor ao exponente escolhido
+nota.toExponencial(2);//100
+//Retorna o valor com o numero de casas decimais escolhido
+nota.toFixed(2);//10.00
+nota.toPrecision(1);//1e+1
+//Transforma o valor em uma String
+nota.toString();//"10"
+//Retorna o valor presente na variavel
+nota.valueOf();//10
