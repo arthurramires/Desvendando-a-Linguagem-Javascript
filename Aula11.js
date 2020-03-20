070f787a303d592a8213156d8594fd003487d579
arthurramires <arthurramires1802@outlook.com>
2020-03-20 09:21:27 -0400

    --Aula 11- Date

---
+++ Aula11.js
@@ -0,0 +1,13 @@
+//Aula 11 - Date
+var hoje = new Date();
+//Pegar o tempo em milissegundos
+hoje.getTime();
+new Date("2014-12-25T10:30:00-02:00")
+//Pegar o ano
+hoje.getFullYear();//2020
+//Retorna o dia
+hoje.getDay();
+//Retorna as horas
+hoje.getHours();
+//Retorna a data em String
+hoje.toString();//
